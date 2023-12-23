4b463186ee4028ee707b5a3a235ca4c43ef3f19a
Dmitri Tcherevik <cherevik@yahoo.com>
2023-12-23 06:31:30 -0500

    Use the latest anthropic messaging api

--- aq/providers/anthropic/provider.py
+++ aq/providers/anthropic/provider.py
@@ -1,22 +1,30 @@
 import logging
-from typing import Dict, Any
+from typing import Dict, Any, Literal, List
 
 from pydantic import BaseModel
 
 from ..provider import BaseProvider, ProviderError
-from ..types import ChatCompletionRequest, ChatCompletionResponse, Choice, ChatCompletionMessage
+from ..types import ChatCompletionRequest, ChatCompletionResponse, Choice, ChatCompletionMessage, Content, Error
 from ...http_client import AsyncHttpClient
+
+
+class Message(BaseModel):
+    role: Literal["user", "assistant"]
+    content: str
 
 
 class AnthropicCompletionRequest(BaseModel):
     model: str
-    prompt: str
+    messages: List[Message]
+    system: str
     temperature: float
-    max_tokens_to_sample: int
+    max_tokens: int
 
 
 class AnthropicCompletionResponse(BaseModel):
-    completion: str
+    type: Literal["message"]
+    role: Literal["assistant"]
+    content: List[Content]
     model: str
     stop_reason: str
 
@@ -27,51 +35,60 @@ class AnthropicProvider(BaseProvider):
         self._http_client = http_client
         self._logger = logging.getLogger(self.__class__.__name__)
 
+    async def create_completion(self, request: ChatCompletionRequest) -> ChatCompletionResponse:
+        self._check_config(self._config)
+
+        # Format the prompt
+        messages = []
+        system_prompts = []
+        for message in request.messages:
+            if message.role == "system":
+                system_prompts.append(message.content)
+            else:
+                messages.append(self._msg2msg(message))
+
+        # Map and send the request
+        a_request = AnthropicCompletionRequest(model=request.model,
+                                               messages=messages,
+                                               system="\n".join(system_prompts) if system_prompts else None,
+                                               temperature=request.temperature,
+                                               max_tokens=request.max_tokens)
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "x-api-key": self._config['key'],
+            "anthropic-version": "2023-06-01",
+            "anthropic-beta": "messages-2023-12-15"
+        }
+        url = self._http_client.urljoin(self._config["endpoint"], "/messages")
+        data = await self._http_client.post(url, headers, a_request.model_dump_json(exclude_none=True))
+
+        if "error" in data:
+            error = Error(**data['error'])
+            raise ProviderError(error.code, error.message)
+        else:
+            # Map and return the response
+            a_response = AnthropicCompletionResponse(**data)
+            finish_reason = "stop" if a_response.stop_reason == "end_turn" else a_response.stop_reason
+            if a_response.content:
+                message = ChatCompletionMessage(role="assistant", content=a_response.content[0].text)
+                choice = Choice(index=0, message=message, finish_reason=finish_reason)
+                return ChatCompletionResponse(id="", object="object", created=0, choices=[choice])
+            else:
+                raise ProviderError(500, "Could not parse response")
+
     @staticmethod
     def _check_config(config: Dict[str, Any]) -> None:
         required_keys = ['endpoint', 'key']
         if not all(key in config for key in required_keys):
             raise ProviderError(400, "The Anthropic provider is not configured. Add settings to config.yml.")
 
-    async def create_completion(self, request: ChatCompletionRequest) -> ChatCompletionResponse:
-        self._check_config(self._config)
-
-        # Format the prompt
-        system_prompts = []
-        prompts = []
-        for message in request.messages:
-            if message.role == "system":
-                system_prompts.append(message.content)
-            elif message.role == "user":
-                prompts.append(f"Human: {message.content}")
-            elif message.role == "assistant":
-                prompts.append(f"Assistant: {message.content}")
-            else:
-                self._logger.error(f"Unknown role {message.role}")
-
-        system_prompts.append("")
-        prompts.append("Assistant:")
-        prompt = "\n".join(system_prompts) + "\n\n" + "\n\n".join(prompts)
-
-        # Map and send the request
-        a_request = AnthropicCompletionRequest(model=request.model,
-                                               prompt=prompt,
-                                               temperature=request.temperature,
-                                               max_tokens_to_sample=request.max_tokens)
-        headers = {
-            "Accept": "application/json",
-            "Content-Type": "application/json",
-            "x-api-key": self._config['key'],
-            "anthropic-version": "2023-06-01"
-        }
-        url = self._http_client.urljoin(self._config["endpoint"], "/complete")
-        data = await self._http_client.post(url, headers, a_request.model_dump_json(exclude_none=True))
-
-        # Map and return the response
-        a_response = AnthropicCompletionResponse(**data)
-        finish_reason = "stop" if a_response.stop_reason == "stop_sequence" else a_response.stop_reason
-        message = ChatCompletionMessage(role="assistant", content=a_response.completion)
-        choice = Choice(index=0, message=message, finish_reason=finish_reason)
-        response = ChatCompletionResponse(id="", object="object", created=0, choices=[choice])
-
-        return response
+    @staticmethod
+    def _msg2msg(message: ChatCompletionMessage) -> Message:
+        if isinstance(message.content, str):
+            return Message(role=message.role, content=message.content)
+        elif isinstance(message.content, list):
+            content = "\n".join([c.text for c in message.content if c.type == "text"])
+            if content:
+                return Message(role=message.role, content=content)
+        raise ProviderError(400, "Invalid message")
